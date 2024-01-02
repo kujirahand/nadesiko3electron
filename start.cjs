73e70016fbed92790aab3e5210cd7b51ddd3bc2d
kujirahand <web@kujirahand.com>
2024-01-02 18:15:24 +0900

    update start process for win

--- start.cjs
+++ start.cjs
@@ -1,10 +1,12 @@
 const path = require('path')
 global.require = require
 global.__dirname = __dirname
-const a_path = path.join(__dirname, 'src', 'enako3.mjs')
-console.log("[start.cjs] path=", a_path)
 try {
-    import(a_path)
+    import('./src/enako3.mjs').then(() => {
+        console.log("[start.cjs] ok.")
+    }).catch((e) => {
+        console.log('[ERROR] start.cjs', e)
+    })
 } catch (e) {
     console.log('[ERROR] start.cjs', e)
 }
