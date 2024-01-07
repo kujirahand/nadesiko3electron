7f832409622fc1e6b5d30080ee5fc9a2e01b8ee6
kujirahand <web@kujirahand.com>
2024-01-07 20:00:06 +0900

    fixed batch/update script

--- batch/update-release-mac.sh
+++ batch/update-release-mac.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd)"
 ROOT_PATH="$(cd "$SCRIPT_PATH/.." && pwd)"
-
+NAKO3_PATH="$(cd "$SCRIPT_PATH/../../nadesiko3" && pwd)"
 
 # UPDATE WEB version
+echo "--- SCRIPT_PATH: $SCRIPT_PATH"
+echo "--- ROOT_PATH: $ROOT_PATH"
 rm -f $ROOT_PATH/webapp/nadesiko3/*
-cd ../nadesiko3
+
+echo "--- NAKO3_PATH: $NAKO3_PATH"
+cd $NAKO3_PATH
+npm run hello
 npm run build
-cp ./release/* $ROOT_PATH/webapp/nadesiko3/
+cp $NAKO3_PATH/release/* $ROOT_PATH/webapp/nadesiko3/
+ls $ROOT_PATH/webapp/nadesiko3/*
+
